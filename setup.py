8632bd62c9436bd6f10a56dc0db34d3e8b9240da
David Smith <smithdc@gmail.com>
2022-07-16 17:37:02 +0100

    Project Update
    * Added tox matrix and updated GitHub Actions CI to use it.
    * Added lint
    * Removed Python files, files are unchanged from core crispy-forms
    * Moved tests
    * Updated package name to use underscores

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='crispy-forms-bootstrap2',
-    version='0.1',
+    name="crispy-forms-bootstrap2",
+    version="0.1",
     packages=find_packages(),
-    url='',
-    license='MIT',
-    author='',
-    author_email='',
-    description='Django-crispy-forms bootstrap2 templates',
-    install_requires=['django-crispy-forms >= 1.8'],
+    url="https://github.com/django-crispy-forms/crispy-forms-bootstrap2",
+    license="MIT",
+    description="Django-crispy-forms bootstrap2 templates",
+    install_requires=["django-crispy-forms >= 1.8"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent",        
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
     ],
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
 )
