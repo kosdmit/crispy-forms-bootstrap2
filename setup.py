b4e4657b84dbd43b973324cf785ffa0a4472c432
David Smith <39445562+smithdc1@users.noreply.github.com>
2024-01-27 15:30:19 +0000

    2024.1 Release (#9)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="crispy-forms-bootstrap2",
-    version="2022.1",
+    version="2024.1",
     packages=find_packages(),
     url="https://github.com/django-crispy-forms/crispy-forms-bootstrap2",
     license="MIT",
